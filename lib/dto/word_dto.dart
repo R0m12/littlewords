b7a3d2f90439798e1aa724790c498c77e3bdc99a
Kojox23 <demaille.lucas@gmail.com>
2022-11-30 11:53:01 +0100

    Init repo

---
+++ lib/dto/word_dto.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'word_dto.g.dart';
+
+@JsonSerializable()
+class WordDTO{
+  WordDTO(this.uid, this.author, this.content, this.latitude, this.longitude);
+
+  final int? uid;
+  final String? author;
+  final String? content;
+  final String? latitude;
+  final String? longitude;
+
+  Map<String, dynamic> toJson() => _$WordDTOToJson(this);
+
+  factory WordDTO.fromJson(Map<String, dynamic> json) => _$WordDTOFromJson(json);
+}
