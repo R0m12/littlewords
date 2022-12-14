628496da9ec206b9934d9c4804c2a72c0412355c
Cofan <coco59dk@hotmail.fr>
2022-12-14 12:56:08 +0100

    Modification du fichier liste_mots.dart : ajout d'icones à la place des boutons

--- lib/home/liste_mots.dart
+++ lib/home/liste_mots.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:littlewords/db/dbHelper.dart';
 
 class ListeMots extends StatelessWidget {
+  const ListeMots({Key? key}): super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         Wrap(
           children: [
+            IconButton(onPressed: (){
+              //TODO: Supprimer le mot de la base de données
+                print("SUPPRIME");
+              },
+              icon: Icon(Icons.delete_forever),
+
+            ),
+            IconButton(onPressed: (){
+              //TODO: Redéposer le mot sur la carte à l'emplacement de l'utilisateur
+                print("DEPOSE");
+              },
+              icon: Icon(Icons.undo),
+            ),
             ElevatedButton(onPressed: (){
               Navigator.pop(context);
             }, child: Text('Déposer')),
