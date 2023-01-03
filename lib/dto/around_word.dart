805c2ff754833e531bcbefd1f9185196c4705f80
Kojox23 <demaille.lucas@gmail.com>
2023-01-03 15:54:37 +0100

    Modif Lucas

---
+++ lib/dto/around_word.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:littlewords/dto/word_dto.dart';
+
+class WordsAroundMarketLayer extends ConsumerWidget {
+  const WordsAroundMarketLayer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ref
+        .watch(wordsAroundProvider)
+        .when(data: _onData, error: _onError, loading: _onLoading);
+  }
+
+  Widget _onData(List<WordDTO> words){
+    final List<Market> markers = <Marker>[];
+    for (final WordDTO w in words){
+      var wordPosition = LatLng(w.latitude!, w.longitude!);
+      markers.add(Marker(
+        point : wordPosition,
+        builder : (context) {
+          return _WordMarkerContent(w);
+        }
+      ));
+    }
+    return MarkerLayer(markers: markers);
+  }
+
+  GestureDetector _WordMakerContent(WordDTO w){
+    return GestureDetector(
+      onTap: ()=> print('tapped ${w.uid}'),
+      child: const Icon(
+        Icons.message,
+        size: 32,
+      ),
+    );
+  }
+  Widget _onLoading(){
+    return const Center(child: CircularProgressIndicator());
+  }
+
+  Widget _onError(error, stack){
+    print(error);
+    return Container(
+      color: Colors.red.withOpacity(0.5),
+    );
+  }
+}
